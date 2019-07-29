4207f091ca1fb29ace602f23cbc0354ab02d908f
Robert Bricheno <github2@ambrosia.org.uk>
2019-07-29 15:55:32 +0100

    Update example.c

--- example.c
+++ example.c
@@ -3,7 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* makeRak (uint8_t* inputBuffer, int inputSize) {
+/*
+ * Each ASCII byte is encoded as two hexadecimal digits.
+ * The resulting buffer is suitable for use as "datahex" in
+ * function rk_sendData(type, port, datahex) seen here:
+ * https://github.com/RAKWireless/RAK811/blob/master/Arduino%20Library/RAK811/RAK811.cpp
+ */
+char* rk_dataHex(uint8_t* inputBuffer, int inputSize) {
   char buildBuffer[4] = {0};
   int i;
   char* compositionBuffer = (char *) malloc(inputSize*2+1);
@@ -17,10 +23,10 @@ char* makeRak (uint8_t* inputBuffer, int inputSize) {
 int main(void) {
   // uint8_t* inputBuffer = lpp.getBuffer();
   // int inputSize = lpp.getSize();
-  char a[]="ABCD";
+  char a[] = "ABCD";
   uint8_t* inputBuffer = (uint8_t*)a;
   int inputSize = sizeof(inputBuffer);
-  char* p = makeRak(inputBuffer, inputSize);
+  char* p = rk_dataHex(inputBuffer, inputSize);
   puts(p);
   free(p);
 }
