c9c5117d4ff3358259ca614d778089f4449f1738
Robert Bricheno <github2@ambrosia.org.uk>
2019-07-30 14:53:16 +0100

    Improve speed

--- example.c
+++ example.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <string.h>
+
+const char *hexdigits = "0123456789ABCDEF";
 
 /*
  * Each ASCII byte is encoded as two hexadecimal digits.
@@ -9,15 +10,17 @@
  * function rk_sendData(type, port, datahex) seen here:
  * https://github.com/RAKWireless/RAK811/blob/master/Arduino%20Library/RAK811/RAK811.cpp
  */
-char* rk_dataHex(uint8_t* inputBuffer, int inputSize) {
-  char buildBuffer[4] = {0};
-  int i;
-  char* compositionBuffer = (char *) malloc(inputSize*2+1);
-  for (i = 0; i < inputSize; i++) {
-    sprintf(buildBuffer, "%02X", (uint8_t)inputBuffer[i]);
-    strcat(compositionBuffer, buildBuffer);
-  }
-  return compositionBuffer;
+char* makeRak (uint8_t* inputBuffer, int inputSize) {
+    int i, j;
+    char* compositionBuffer = (char *) malloc(inputSize*2+1);
+    for (i = j = 0; i < inputSize; i++) {
+        unsigned char c;
+        c = (inputBuffer[i] >> 4) & 0xf;
+        compositionBuffer[j++] = hexdigits[c];
+        c = inputBuffer[i] & 0xf;
+        compositionBuffer[j++] = hexdigits[c];
+    }
+    return compositionBuffer;
 }
 
 int main(void) {
